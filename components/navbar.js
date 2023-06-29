fa07fafdca5a565b328d16f20ddbcf7bbbad5a14
Anisha Hirani <anisha.hirani11@gmail.com>
2023-06-29 12:43:05 +0100

    added a navbar with text

---
+++ components/navbar.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+
+const navbar = () => {
+  return (
+    <nav>
+      <li>
+        <Link href='/'>
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href='/about'>
+          <a>About</a>
+        </Link>
+      </li>
+      <li>
+        <Link href='/gallery'>
+          <a>Gallery</a>
+        </Link>
+      </li>
+      <li>
+        <Link href='/calendar'>
+          <a>Calendar</a>
+        </Link>
+      </li>
+      <li>
+        <Link href='/contact'>
+          <a>Contact</a>
+        </Link>
+      </li>
+    </nav>
+  )
+}
+
+export default navbar;
